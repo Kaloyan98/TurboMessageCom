3d37e6a737a607ca06e275c68bd4b8ccf8152ce7
Kaloyan98 <kaloqn665@gmail.com>
2018-10-04 20:50:40 +0300

    Add inital dependencies

---
+++ checklist.md
@@ -0,0 +1,4 @@
+### Communication Message System
+
+- [ ] Create a modal for the messages
+- [ ] Setup the elequent database so it suits your needs
---
+++ resources/builds/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { ProvidePlugin } = require('webpack');
+
+const babelLoader = {
+	loader: 'babel-loader',
+	options: {
+		comments: false,
+		presets: [
+			'stage-2'
+		]
+	}
+};
+
+
+const plugins = [
+	new ProvidePlugin({
+		$: 'jquery',
+		jQuery: 'jquery',
+	})
+];
+
+// when going to push to production
+// set to uglify js
+if (false) {
+	plugins.push(
+		new UglifyJSPlugin()
+	);
+}
+
+
+module.exports = {
+	entry: './resources/js/main.js',
+
+	output: {
+		filename: 'bundle.js'
+	},
+
+	resolve: {
+		modules: ['node_modules'],
+		alias: {
+			masonry: 'masonry-layout',
+			isotope: 'isotope-layout',
+			'jquery-ui': 'jquery-ui-distjquery-ui.js'
+		}
+	},
+
+	externals: {
+		jquery: 'jQuery'
+	},
+
+
+	module: {
+		rules: [
+			{
+				test: /.js$/,
+				exclude: /node_modules/,
+				use: [babelLoader]
+			}
+		],
+	},
+
+
+	plugins,
+};
