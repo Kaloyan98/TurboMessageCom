3ef0444e61917441d74e87930555d0d3e0935651
Kaloyan98 <kaloqn665@gmail.com>
2018-10-07 20:44:33 +0300

    Add toggle for message section

--- resources/js/vue/components/icon.js
+++ resources/js/vue/components/icon.js
@@ -1,9 +1,24 @@
 import Vue from 'vue';
 
+import { mapActions, mapGetters } from 'vuex';
+
 Vue.component('tcm-icon', {
+	computed: {
+		...mapGetters('messenger', [
+			'isMessageSectionShown'
+		]),
+	},
 	data() {
 		return {
-
+			localShowMessageSection
 		};
+	},
+	methods: {
+		...mapActions('messenger', [
+			'showMessageSection'
+		]),
+		toggleMessageSection() {
+			this.showMessageSection(!this.isMessageSectionShown);
+		},
 	}
 });
--- templates/messenger/icon.php
+++ templates/messenger/icon.php
@@ -1,6 +1,6 @@
 <tcm-icon inline-template>
 	<div class="section-message-icon">
-		<a class="message-icon-container" href="#">
+		<a class="message-icon-container" href="#" @click.prevent="toggleMessageSection">
 			<i class="ico-message"></i>
 		</a>
 	</div>
